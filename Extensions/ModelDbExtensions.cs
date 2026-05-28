828ae03780c32a1eb4b487bfe03bc072ef2aa8ee
Alchyr <isithalchyr@gmail.com>
2026-05-28 02:34:57 -0700

    add extension method to get cardmodifier instance

---
+++ Extensions/ModelDbExtensions.cs
@@ -0,0 +1,21 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Models;
+
+namespace BaseLib.Extensions;
+
+public static class ModelDbExtensions
+{
+    //Will require language version set to 14 to use.
+    extension(ModelDb)
+    {
+        public static T CardModifier<T>() where T : CardModifier
+        {
+            return ModelDb.Get<T>();
+        }
+    }
+
+    public static T GetCardModifier<T>() where T : CardModifier
+    {
+        return ModelDb.CardModifier<T>();
+    }
+}
